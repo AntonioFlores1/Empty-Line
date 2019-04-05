ae4d2c45a4fc04f4eca1290f1d1557afa61a848c
AntonioFlores1 <af513249@gmail.com>
2019-04-05 12:29:20 -0400

    set up gitignore and cleaned up our file

---
+++ EmptyLine/Shopping /ShoppingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShoppingTableViewCell.swift
+//  EmptyLine
+//
+//  Created by Pursuit on 4/5/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
