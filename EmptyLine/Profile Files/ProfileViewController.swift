af1b24242701b9c7cc9fed4d7bc21b192c7cabfa
josealarconchacon <jose.alarcon.chacom@gmail.com>
2019-04-08 21:55:47 -0400

    working on the cell

--- EmptyLine/Profile Files/ProfileViewController.swift
+++ EmptyLine/Profile Files/ProfileViewController.swift
@@ -22,27 +22,27 @@ class ProfileViewController: UIViewController {
                     "Thursday - 03/15/2019",
                     "Friday - 03/25/2019",
                     "Saturday - 04/01/2019",]
+    var newA = ["Monday - 02/10/2019",
+                    "Tuesday - 02/20/2019",
+                    "Wednesday - 03/5/2019",]
     
-    var array = ["Monday",
-                    "Tuesday",
-                    "Wednesday ",
-                    "Thursday"]
+    var array = ["Name", "Email", "Password", "SingOut"]
     
+    var tapGRec = UITapGestureRecognizer()
     private var profileView = ProfileView()
     private var settingImage: ImageToEdit?
     private var selectedImage: UIImage?
 
     
-    static var authservice = AppDelegate.authservice
-    
-    private var userInfo = [CCUser]() {
+    private let authservice = AppDelegate.authservice
+    public var username: CCUser!
+    private var userHistoryInfo = [CCUser]() {
         didSet {
             DispatchQueue.main.async {
                 self.tableView.reloadData()
             }
         }
     }
-    
     
     lazy var imagePicker: UIImagePickerController = {
         let imagePicker = UIImagePickerController()
@@ -65,69 +65,71 @@ class ProfileViewController: UIViewController {
         tableView.delegate = self
         tableViewconstriant()
         tableView.register(TableViewCell.self, forCellReuseIdentifier: "cell")
-        tableView.register(TableViewCell.self, forCellReuseIdentifier: "Cell")
-        navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "Payment Ray", style: .done, target: self, action: #selector(segueforRaymond))
-        navigationItem.rightBarButtonItem = UIBarButtonItem.init(title: "Payment Ray", style: .done, target: self, action: #selector(segueToSetting))
+        tableView.register(SettingTableViewCell.self, forCellReuseIdentifier: "settinCell")
         profileView.segmentedControl.addTarget(self, action: #selector(segmentedControlPress(_:)), for: .valueChanged)
+        tapGRec = UITapGestureRecognizer(target: self, action: #selector(handleTap(gestureRecognizer:)))
+        profileView.profileImageView.addGestureRecognizer(tapGRec)
+        profileView.profileImageView.isUserInteractionEnabled = true
+        fetchUser()
+//        signOutPressed(self)
+    }
+    func signOutPressed(_ sender: Any) {
+        authservice.signOutAccount()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        fetchUser()
+    }
+    func fetchUser() {
+        guard let user = authservice.getCurrentUser() else {
+            print("no logged user")
+            return
+        }
+        DBService.fetchUser(userId: user.uid) { (error, ccuser) in
+            if let error = error {
+                self.showAlert(title: "Error fetching user", message: error.localizedDescription)
+            } else if let ccuser = ccuser {
+                self.profileView.usernameLabel.text = ccuser.fullName
+                self.username = ccuser
+                guard let photoURl = ccuser.photoURL, !photoURl.isEmpty else {return}
+                self.profileView.profileImageView.kf.setImage(with: URL(string: photoURl))
+            }
+        }
     }
     
-    
-    @objc func segmentedControlPress(_ sender: UISegmentedControl) {
-        self.tableView.reloadData()
-    }
-    
-    @objc func editButtonPressw() {
-        print("Press")
+     @objc private func handleTap(gestureRecognizer: UITapGestureRecognizer) {
+        print("Tap")
         settingImage = .profileImage
         let alertController = UIAlertController(title: "Edit Profile", message: nil, preferredStyle: .actionSheet)
         let camera = UIAlertAction(title: "Camera", style: .default) { (action) in
             self.imagePicker.sourceType = .camera
             self.showImagePickerController()
+            self.profileView.defaultCamera.isHidden = true
         }
         let photoLibrary = UIAlertAction(title: "PhotoLibrary", style: .default) { (action) in
             self.imagePicker.sourceType = .photoLibrary
             self.showImagePickerController()
+            self.profileView.defaultCamera.isHidden = true
         }
-        let nameToEdit = UIAlertAction(title: "Change your name", style: .default) { (action) in
-            let textFieldAlert = UIAlertController(title: "Change your name", message: "Enter your name", preferredStyle: .alert)
-            textFieldAlert.addTextField(configurationHandler: { (email: UITextField) in
-                // code will be here
-                email.placeholder = "enter your name"
-            })
-            let change = UIAlertAction(title: "Change", style: .default, handler: { (action) in
-                let name = textFieldAlert.textFields![0] as! UITextField
-                self.profileView.nameLabel.text = name.text
-                self.dismiss(animated: true, completion: nil)
-            })
-            let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
-                self.dismiss(animated: true, completion: nil)
-            })
-            textFieldAlert.addAction(change)
-            textFieldAlert.addAction(cancel)
-            self.present(textFieldAlert, animated: true, completion: nil)
-            //=====--
-        }
-        let cancel = UIAlertAction(title: "cancel", style: .cancel) { (action) in
-        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: { (action) in
+            self.dismiss(animated: true, completion: nil)
+        })
         alertController.addAction(camera)
         alertController.addAction(cancel)
         alertController.addAction(photoLibrary)
-        alertController.addAction(nameToEdit)
         present(alertController, animated: true, completion: nil)
+
+    }
+    
+    @objc func segmentedControlPress(_ sender: UISegmentedControl) {
+        self.tableView.reloadData()
     }
     
     private func showImagePickerController() {
         present(imagePicker,animated: true,completion:  nil)
     }
     
-    @objc func segueforRaymond(){
-        navigationController?.pushViewController(CreditCardInfoSetupViewController(), animated: true)
-    }
-    @objc func segueToSetting() {
-//        navigationController?.pushViewController(SettingsViewController(), animated: true)
-        
-    }
-    
+  
     func tableViewconstriant() {
         self.view.addSubview(tableView)
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -139,12 +141,45 @@ class ProfileViewController: UIViewController {
 }
 
 extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        let title = array[section]
+//        return title
+        switch profileView.segmentedControl.selectedSegmentIndex {
+        case 0:
+            return ""
+        case 1:
+            return array[section]
+            if (section == 0) {
+                return ""
+            }
+            if section == 1 {
+                return ""
+            }
+            if section == 2 {
+                return ""
+            }
+        default:
+            break
+        }
+        return ""
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return array.count
+    }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch profileView.segmentedControl.selectedSegmentIndex {
         case 0:
             return newArray.count
         case 1:
-            return array.count
+            if (section == 0) {
+                return 1
+            }
+            if section == 1 {
+                return 2
+            }
+            if section == 2 {
+                return 1
+            }
         default:
             break
         }
@@ -152,20 +187,23 @@ extension ProfileViewController: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let info = userInfo[indexPath.row]
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? TableViewCell else { return UITableViewCell()}
-            switch profileView.segmentedControl.selectedSegmentIndex {
-            case 0:
-                cell.historyLabel.text = newArray[indexPath.row]
-            case 1:
-               cell.historyLabel.text = array[indexPath.row]
-            default:
-                break
+        switch profileView.segmentedControl.selectedSegmentIndex {
+        case 0:
+            cell.historyLabel.text = newArray[indexPath.row]
+        case 1:
+            cell.emailLabel.text = newA[indexPath.row]
+            cell.historyImage.isHidden = true
+            cell.historyLabel.isHidden = true
+        default:
+            break
         }
         return cell
-    }
+        }
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
+        return 60
     }
 }
 
--- EmptyLine/Profile Files/SettingTableViewCell.swift
+++ EmptyLine/Profile Files/SettingTableViewCell.swift
@@ -1,52 +1,33 @@
 //
-//  TableViewCell.swift
+//  SettingTableViewCell.swift
 //  EmptyLine
 //
-//  Created by Jose Alarcon Chacon on 4/6/19.
+//  Created by Jose Alarcon Chacon on 4/8/19.
 //  Copyright © 2019 Pursuit. All rights reserved.
 //
 
 import UIKit
 
-class TableViewCell: UITableViewCell {
+class SettingTableViewCell: UITableViewCell {
 
-    lazy var  historyLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .black
-        return label
-    }()
-    
-    lazy var historyImage: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "placeholder")
-        image.layer.cornerRadius = image.frame.width/2
-        image.clipsToBounds = true
-        return image
-    }()
-    
     lazy var  namelLabel: UILabel = {
         let name = UILabel()
         name.textColor = .black
         return name
     }()
-
+    
     lazy var  emailLabel: UILabel = {
         let email = UILabel()
         email.textColor = .black
         return email
     }()
-
+    
     lazy var  passwordLabel: UILabel = {
         let pass = UILabel()
         pass.textColor = .black
         return pass
     }()
     
-    lazy var signOut: UIButton = {
-        let out = UIButton()
-        return out
-    }()
-
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -63,33 +44,12 @@ class TableViewCell: UITableViewCell {
     }
     
     func setUpViewConstraints() {
-        addSubview(historyLabel)
-        addSubview(historyImage)
         addSubview(emailLabel)
         addSubview(namelLabel)
         addSubview(passwordLabel)
-        addSubview(signOut)
-        historyLabelConstraints()
-        historyImageConstraints()
         emailConstraints()
         nameLabelConstraints()
         passwordLabelConstraints()
-    }
-    
-    func historyLabelConstraints() {
-        historyLabel.translatesAutoresizingMaskIntoConstraints = false
-        historyLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 40).isActive = true
-        historyLabel.leadingAnchor.constraint(equalTo: historyImage.trailingAnchor, constant: 10).isActive = true
-        historyLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
-    }
-    func historyImageConstraints() {
-        historyImage.translatesAutoresizingMaskIntoConstraints = false
-        historyImage.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 10).isActive = true
-        historyImage.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20).isActive = true
-        historyImage.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -300).isActive = true
-        historyImage.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -10).isActive = true
-        historyImage.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        historyImage.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     func emailConstraints() {
         emailLabel.translatesAutoresizingMaskIntoConstraints = false
@@ -108,11 +68,5 @@ class TableViewCell: UITableViewCell {
         passwordLabel.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
         passwordLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 11).isActive = true
         passwordLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -11).isActive = true
-    }
-    func sigOutonstraints() {
-        signOut.translatesAutoresizingMaskIntoConstraints = false
-        signOut.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-        signOut.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 11).isActive = true
-        signOut.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -11).isActive = true
     }
 }
