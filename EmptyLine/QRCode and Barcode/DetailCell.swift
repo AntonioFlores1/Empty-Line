03c1723773ecec25cfe550cebb9a250c6a7a1145
AlfredoB212 <alfredobarragan@Pursuits-MacBook-Pro-93.local>
2019-04-11 12:27:03 -0400

    creating the cell for the detailitemview

--- EmptyLine/QRCode and Barcode/DetailCell.swift
+++ EmptyLine/QRCode and Barcode/DetailCell.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2019 Pursuit. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class DetailCell: UICollectionViewCell {
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Name of item"
+        return label
+    }()
+    
+    let priceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Price of item"
+        return label
+    }()
+    
+    let itemImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "item")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    
+}
