0e263220d582ec3e4cdea19d62406bc88add0b87
AntonioFlores1 <af513249@gmail.com>
2019-04-15 15:36:28 -0400

    changes made

--- EmptyLine/ProductDetailViewController.swift
+++ EmptyLine/ProductDetailViewController.swift
@@ -11,25 +11,6 @@ import Firebase
 import FirebaseMLVision
 
 class ProductDetailViewController: UIViewController {
-<<<<<<< HEAD
-    
-    var productDetailView = ProductDetailsView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.addSubview(productDetailView)
-        dismissButton()
-     
-    }
-    func dismissButton() {
-        productDetailView.deleteButton.addTarget(self, action: #selector(dismissButtonPressed), for: .touchUpInside)
-    }
-    
-    @objc private func dismissButtonPressed(){
-        dismiss(animated: true, completion: nil)
-        print("dismiss")
-    }
-=======
 
 //
 //    private var productDetailView = ProductDetailsView()
@@ -124,6 +105,4 @@ class ProductDetailViewController: UIViewController {
 //
 //        }
 //    }
-  
->>>>>>> ad2017fde4b78552a62c5cc34d18cfa58af42be4
-}
+  }
