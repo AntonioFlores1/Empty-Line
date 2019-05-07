a0195978e281f970aea696dff79487fd1b921e75
josealarconchacon <jose.alarcon.chacom@gmail.com>
2019-05-07 13:17:41 -0400

    Fixed pay button and receip.

--- EmptyLine/Shopping /ShoppingListViewController.swift
+++ EmptyLine/Shopping /ShoppingListViewController.swift
@@ -90,8 +90,13 @@ class ShoppingListViewController: UIViewController {
         activityView.center = shoppingView.payButton.center
         //self.activityView.layer.addSublayer(gradient)
         view.addSubview(activityView)
-
-        
+    }
+    private func controlPayButton() {
+        if shoppingCart.isEmpty == true {
+            shoppingView.payButton.isEnabled = false
+        } else if shoppingCart.isEmpty != true {
+            shoppingView.payButton.isEnabled = true
+        }
     }
  
     @objc func payButtonPressed() {
@@ -101,6 +106,7 @@ class ShoppingListViewController: UIViewController {
    
     override func viewWillAppear(_ animated: Bool) {
         fetchShoppingCartItems()
+        controlPayButton()
         shoppingView.shoppingListTableView.reloadData()
         shoppingView.payButton.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
         UIView.animate(withDuration: 2.0,delay: 0,usingSpringWithDamping: 0.2,initialSpringVelocity: 6.0, options: .allowUserInteraction, animations: { [weak self] in
@@ -263,6 +269,7 @@ extension ShoppingListViewController{
         if editingStyle == .delete {
             print("Deleted")
             self.shoppingCart.remove(at: indexPath.row)
+            self.shoppingView.payButton.isEnabled = false
             self.shoppingView.shoppingListTableView.deleteRows(at: [indexPath], with: .automatic)
             ShoppingCartDataManager.deleteItemFromShoppingCart(index: indexPath.row)
             self.itemsPriceTotal = ShoppingCartDataManager.totalAmount()
@@ -284,13 +291,15 @@ extension ShoppingListViewController: STPAddCardViewControllerDelegate {
         dismiss(animated: true, completion: nil)
         showAlert(title: "\(authservice.getCurrentUser()?.displayName ?? "") Your transaction was successful. \n $\(Float(itemsPriceTotal)) will be taken from your card", message: "Thank you for shopping with zipLine") { (alert) in
             self.itemsPriceTotal = 0.0
+            self.shoppingCart.removeAll()
+            self.refresh.endRefreshing()
             self.barButtonItem.isEnabled = false
             self.createShoppingHistory()
             ReceiptDataManager.addToCheckoutItems(items: self.shoppingCart)
            
             ShoppingCartDataManager.deleteAllItems()
-            self.shoppingCart.removeAll()
-            self.refresh.endRefreshing()
+//            self.shoppingCart.removeAll()
+//            self.refresh.endRefreshing()
    
             self.navigationController!.pushViewController(ReceiptViewController(), animated: true)
 
