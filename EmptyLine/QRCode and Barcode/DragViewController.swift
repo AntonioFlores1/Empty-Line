602da7c5415268888a45e8632c33f93b54e6a8c0
AntonioFlores1 <af513249@gmail.com>
2019-05-09 13:25:28 -0400

    shopping cart badge created and animation done

--- EmptyLine/QRCode and Barcode/DragViewController.swift
+++ EmptyLine/QRCode and Barcode/DragViewController.swift
@@ -19,62 +19,29 @@ class DragViewController: UIViewController {
     
     @IBOutlet weak var itemPrice: UILabel!
     
-    @IBOutlet weak var addToCartAnimation: LottieView!
-    
     @IBOutlet weak var dontAddCartAnimation: LottieView!
     
-    
-    @IBOutlet weak var addButtonToCart: UIButton!
+    @IBOutlet weak var addCartAni: LottieView!
     
     @IBOutlet weak var dontAdd: UIButton!
     
+    @IBOutlet weak var addButton: UIButton!
+    
     @IBOutlet weak var itemDescription: UITextView!
     
-    //let things = AnimatedButton()
-//things.translatesAutoresizingMaskIntoConstraints = false
-    /// Set an animation on the button.
-//    things.animation = Animation.named("TwitterHeartButton", subdirectory: "TestAnimations")
-    /// Turn off clips to bounds, as the animation goes outside of the bounds.
-//    things.clipsToBounds = false
-    /// Set animation play ranges for touch states
-//    twitterButton.setPlayRange(fromMarker: "touchDownStart", toMarker: "touchDownEnd", event: .touchDown)
-//    twitterButton.setPlayRange(fromMarker: "touchDownEnd", toMarker: "touchUpCancel", event: .touchUpOutside)
-//    twitterButton.setPlayRange(fromMarker: "touchDownEnd", toMarker: "touchUpEnd", event: .touchUpInside)
-//    view.addSubview(twitterButton)
-    let animationView = AnimationView(name: "animation-w800-h600")
+    let animationView = AnimationView(name: "addToCartAni")
     
-    let twitterButton = AnimatedButton()
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        /// Set an animation on the button.
-//        constraints()
-//        twitterButton.backgroundColor = .red
-//        twitterButton.animation = Animation.named("animation-w800-h600")
-        /// Turn off clips to bounds, as the animation goes outside of the bounds.
-        //twitterButton.clipsToBounds = false
-        /// Set animation play ranges for touch states
-       // twitterButton.addTarget(self, action: #selector(thingido), for: .touchUpInside)
-//        twitterButton.setPlayRange(fromMarker: "touchDownStart", toMarker: "touchDownEnd", event: .touchDown)
-//        twitterButton.setPlayRange(fromMarker: "touchDownEnd", toMarker: "touchUpCancel", event: .touchUpOutside)
-//        twitterButton.setPlayRange(fromMarker: "touchDownEnd", toMarker: "touchUpEnd", event: .touchUpInside)
-        //addButton.animation = Animation.named("animation-w800-h600")
-       // animationView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
-       // things.animation = Animation.named("animation-w800-h600")
-        //things.backgroundColor = .red
-       // things.clipsToBounds = true
-       // things.setPlayRange(fromMarker: "touchUpEnd", toMarker: "touchDownStart", event: .touchUpInside)
-       // things.addTarget(self, action: #selector(thingstodo), for: .touchUpInside)
-        //things
-       // things.setPlayRange(fromMarker: "touchDownStart", toMarker: "touchUpEnd", event: .touchUpInside)
-       // view.addSubview(things)
-      //  self.addToCartAnimation.addSubview(animationView)
-
+        addCartAni.addSubview(animationView)
+        animationView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+        animationView.stop()
+        
     }
     @objc func thingstodo(){
         print("i is pressed")
-       // things.setPlayRange(fromProgress: CGFloat(0), toProgress: CGFloat(2), event: .touchUpInside)
+       
 
     }
     
@@ -85,8 +52,8 @@ class DragViewController: UIViewController {
     
     @IBAction func dontAddButton(_ sender: Any) {
 //        dontAdd.isHidden = true
-        let animationView = AnimationView(name: "animation-w64-h64 (1)")
-        animationView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
+//        let animationView = AnimationView(name: "animation-w64-h64 (1)")
+//        animationView.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
         //animationView.contentMode = .scaleAspectFill
        // animationView.loopMode = .loop
         
@@ -97,29 +64,21 @@ class DragViewController: UIViewController {
     }
     
     @IBAction func addToCartButton(_ sender: Any) {
-       // addButtonToCart.isHidden = true
+        animationView.play()
+        //////FIX THIS LATER
+//        dragViewController.view.frame = CGRect(x: 0,
+//                                               y: 900,
+//                                               width: self.view.bounds.width,
+//                                               height: dViewHeight)
         //      animationView.contentMode = .scaleAspectFill
         //        animationView.loopMode = .loop
         //       animationView.realtimeAnimationProgress
        // animationView.forceDisplayUpdate()
         //animationView.stop()
         //animationView.play()
-        //animationView.backgroundBehavior = .pauseAndRestore
+//        //animationView.backgroundBehavior = .pauseAndRestore
         print("add me pressed")
-        
-    }
-    
-    func constraints(){
-        view.addSubview(twitterButton)
-        twitterButton.translatesAutoresizingMaskIntoConstraints = false
-        //twitterButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        //twitterButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        twitterButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 7).isActive = true
-        twitterButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -11).isActive = true
-//        things.heightAnchor.constraint(equalTo: view.heightAnchor, constant: 80).isActive = true
-        twitterButton.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.1).isActive = true
-        twitterButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.2).isActive = true
-//        things.widthAnchor.constraint(equalTo: view.widthAnchor, constant: 80).isActive = true
+//
     }
 
 }
